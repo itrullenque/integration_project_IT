0e41e37e01456c301bbf01af6af77ec3d3a516e6
itrullenque <itrullenques@gmail.com>
2024-03-04 00:14:32 +0100

    Adding oracle DB and methods to get, post/put the portfolio

--- main.py
+++ main.py
@@ -1,28 +1,105 @@
 import requests
 import json
-from flask import Flask
-from flask_cors import CORS
+from flask import Flask, jsonify, request
+from flask_cors import CORS, cross_origin
+import secrets
+import string
+import time
 
 app = Flask(__name__)
 CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 
+#General variables
 apikey = "VFVETDZPXW4IOBLD"
+portfolio_id = "91474d3a-2bcd-4638-985d-089680444d55"
+user_id = "61480832-0556-4176-8a20-f491f2597e96"
+user_name= "Ignacio Trullenque"
 
-client_ticker = []
-def portfolio():
-    with open("portfolio.json") as json_file:
-        data = json.load(json_file)
+def generate_token(length=40):
+    alphabet = string.ascii_letters + string.digits
+    timestamp = str(int(time.time() * 1000))  # Get current time in milliseconds since epoch and convert to string
+    random_chars_length = length - len(timestamp)
+    random_chars = ''.join(secrets.choice(alphabet) for _ in range(random_chars_length))
+    return timestamp + random_chars
+
+#Get the stock from oracle DB - table: USER_STOCKS
+def get_all():
+
+    try:
+        url = "https://g71ab6e0bc037e1-stocktrackerdb.adb.eu-madrid-1.oraclecloudapps.com/ords/admin/user_stocks/" 
+        response = requests.get(url)
+
+        if response.status_code == 200:
+            data = response.json()
+            if "items" in data and len(data["items"])>0:
+                return data["items"]
+            else:
+                raise Exception("Data no found in stock databases")
+        else:
+            response.raise_for_status()
     
-    return data
+    except Exception as e:
+        print("Error:", str(e))
+    
+    return []
 
+#Method to make a put-post request to the table USER_STOCKS
+def put_stocks(props):
+
+    data = get_all()
+    #post - modify a stock that already exist in the database
+    if props["action"] == "modify":
+        for item in data:
+            if item["ticker"] == props["ticker"]:
+                dict_post = {
+                    "stock_id": item["stock_id"],
+                    "portfolio_id": item["portfolio_id"],
+                    "user_id": item["user_id"],
+                    "user_name":item["user_name"],
+                    "ticker": item["ticker"],
+                    "quantity": props["quantity"],
+                    "purchase_price": item["purchase_price"]
+                }
+                break
+
+    #put - create a new object in the database
+    else:
+        dict_post = {
+                    "stock_id": generate_token(),
+                    "portfolio_id": portfolio_id,
+                    "user_id": user_id,
+                    "user_name":user_name,
+                    "ticker": props["ticker"],
+                    "quantity": props["quantity"],
+                    "purchase_price": 0
+                }
+    
+    if dict_post:
+        json_data = json.dumps(dict_post)
+        stock_id = dict_post["stock_id"]
+        try:
+            url = f"https://g71ab6e0bc037e1-stocktrackerdb.adb.eu-madrid-1.oraclecloudapps.com/ords/admin/user_stocks/{stock_id}"
+            response = requests.put(url,data=json_data,headers={"Content-Type": "application/json"})
+
+            if response.status_code == 200:
+                return "Stock modified correctly", 200
+            else:
+                return f"Failed to modify stock. Reason: {response.reason}", response.status_code
+        except Exception as e:
+            print(f"Error: {str(e)}")
+    else:
+        return f"Failed to modify stock"
+
+#Hompage - it render the portfolio
 @app.route('/', methods=["GET"])
 def homepage():
 
-    client_stocks = portfolio()
+    client_stocks = get_all()
     stock_dict    = {}
     total_value = 0
 
-    for item in client_stocks["portfolios"][0]["items"]:
+    for item in client_stocks:
         stock = item["ticker"]
 
         url = f"https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol={stock}&apikey={apikey}"
@@ -40,10 +117,11 @@ def homepage():
     for stock, value in stock_dict.items():
         stock_dict[stock]["weighted_value"] = round((value["total_value"]/total_value)*100,2)
 
-    stock_dict["portfolio_value"] = total_value
+    stock_dict["portfolio_value"] = round(total_value,2)
     
     return stock_dict
 
+#Get the last two month by week of the relevant information of the stocks
 @app.route('/<ticker>', methods=["GET"])
 def ticker_info(ticker):
 
@@ -63,6 +141,7 @@ def ticker_info(ticker):
     except Exception as e:
         print(str(e)) 
 
+#Search bar - if exist a match between the name input by the user and the info od Alphavantage
 @app.route('/search/<ticker>', methods=["GET"])
 def ticker_search(ticker):
 
@@ -87,6 +166,47 @@ def ticker_search(ticker):
     except Exception as e:
         print(str(e))
 
+#Create a new stock or modify a previous one in the portfolio
+@app.route('/edit_stock', methods=["POST", "OPTIONS"])
+@cross_origin()
+def edit_portfolio():
+
+    data = request.get_json()   
+    if data and "action" in data:
+        if data["action"] == "create":
+            if data["newStockName"] != "":
+                new_data = {
+                    "action":data["action"],
+                    "ticker":data["newStockName"],
+                    "quantity":data["newStockQuantity"]
+                }
+                response = ticker_search(new_data["ticker"]) #validate if the stock exist in alphavantage
+            else:
+                return jsonify({"error_code": 201,"message": "Empty ticker"})
+        else:
+            if data["selectedStock"] != "":
+                new_data = {
+                    "action":data["action"],
+                    "ticker":data["selectedStock"],
+                    "quantity":data["quantity"]
+                }
+                response = ticker_search(new_data["ticker"])
+            else:
+                return jsonify({"error_code": 201,"message": "Empty ticker"})
+            
+        #if the stock is exactly the same as a stock in alphavantage we send a post-put
+        key = list(response.keys())[0]
+        if new_data["ticker"] == key:
+            put_response = put_stocks(new_data)
+            if put_response[1] == 200:
+                return jsonify({"error_code": 200,"message": put_response[0]}) 
+            else:
+                return jsonify({"error_code": put_response[1],"message": put_response[0]}) 
+        else:
+            return jsonify({"error_code": 400,"message": "Ticker doesn't exists"})    
+    else:
+        return jsonify({"error_code": 400,"message": "Empty data"})
 
 if __name__ == "__main__":
     app.run(debug=True)
+    
--- test.py
+++ test.py
@@ -1,26 +1,83 @@
 import requests
 import json
 
+import secrets
+import string
+import time
 
-try:
-    apikey = "VFVETDZPXW4IOBLD"
-    stock = "AAPL"
-    url = f"https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords={stock}&apikey={apikey}"
-    
-    response = requests.get(url)
-    value = response.raise_for_status()
-    if response.status_code == 200:
-        data = response.json()
-        if data["bestMatches"] != []:
-            stock_dict = {}
-            for stock in data["bestMatches"]:
-                stock_dict[stock["1. symbol"]] = stock["2. name"]
-            
-            stock_dict
-        else:
-            "No data available for the request stock"
+def generate_token(length=40):
+    alphabet = string.ascii_letters + string.digits
+    timestamp = str(int(time.time() * 1000))  # Get current time in milliseconds since epoch and convert to string
+    random_chars_length = length - len(timestamp)
+    random_chars = ''.join(secrets.choice(alphabet) for _ in range(random_chars_length))
+    return timestamp + random_chars
+
+def put_stocks(props):
+
+    data = get_all()
+    #post - modify a stock that already exist in the database
+    if props["action"] == "modify":
+        for item in data:
+            if item["ticker"] == props["selectedStock"]:
+                dict_post = {
+                    "stock_id": item["stock_id"],
+                    "portfolio_id": item["portfolio_id"],
+                    "user_id": item["user_id"],
+                    "user_name":item["user_name"],
+                    "ticker": item["ticker"],
+                    "quantity": props["quantity"],
+                    "purchase_price": item["purchase_price"]
+                }
+                break
+
+    #put - create a new object in the database
     else:
-        raise Exception("Error obtaining the information of the stocks", "status code {}" .format(response.status_code))
+        portfolio_id = "91474d3a-2bcd-4638-985d-089680444d55"
+        user_id = "61480832-0556-4176-8a20-f491f2597e96"
+        user_name= "Ignacio Trullenque"
+        dict_post = {
+                    "stock_id": "17095042007310IxLhG8qXUfwtW3cjpO3M8sX6dK",#generate_token(),
+                    "portfolio_id": portfolio_id,
+                    "user_id": user_id,
+                    "user_name":user_name,
+                    "ticker": props["selectedStock"],
+                    "quantity": props["quantity"],
+                    "purchase_price": 0
+                }
+    
+    if dict_post:
+        json_data = json.dumps(dict_post)
+        stock_id = dict_post["stock_id"]
+        try:
+            url = f"https://g71ab6e0bc037e1-stocktrackerdb.adb.eu-madrid-1.oraclecloudapps.com/ords/admin/user_stocks/{stock_id}"
+            response = requests.put(url,data=json_data,headers={"Content-Type": "application/json"})
 
-except Exception as e:
-    print(str(e))
+            if response.status_code == 200:
+                return "Stock modified correctly", 200
+            else:
+                return f"Failed to modify stock. Status code: {response.status_code}", response.reason
+        except Exception as e:
+            print(f"Error: {str(e)}")
+
+def get_all():
+
+    try:
+        url = "https://g71ab6e0bc037e1-stocktrackerdb.adb.eu-madrid-1.oraclecloudapps.com/ords/admin/user_stocks/" 
+        response = requests.get(url)
+
+        if response.status_code == 200:
+            data = response.json()
+            if "items" in data and len(data["items"])>0:
+                return data["items"]
+            else:
+                raise Exception("Data no found in stock databases")
+        else:
+            response.raise_for_status()
+    
+    except Exception as e:
+        print("Error:", str(e))
+    
+    return []
+
+props ={"action":"create","selectedStock":"NVDA","quantity":10}
+result = put_stocks(props)
